7e0ffdd1fe3c9aa328ab63a4291086e54491b11a
Declan Chidlow <accounts@vale.rocks>
2025-09-25 16:23:28 +0800

    Add subtle animation

--- sitemap-improver.js
+++
@@ -1,5 +0,0 @@
-export default (sitemapContent) => {
-	if (sitemapContent.includes("<?xml-stylesheet")) return sitemapContent;
-	const withStylesheet = sitemapContent.replace('<?xml version="1.0" encoding="UTF-8"?>', '<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/xsl" href="/assets/sitemap.xsl"?>');
-	return withStylesheet.replace(/(https:\/\/tools\.vale\.rocks[^<\s]+)(\/|\.html)(?=<\/loc>)/g, "$1");
-};
